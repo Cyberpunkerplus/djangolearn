8edb5314194d4f5dd4eee46f3c2a126dd6a79eed
Cyber <dhuwuyi@gmail.com>
2015-05-09 22:12:06 -0400

    start my_blog

--- my_blog/my_blog/settings.py
+++ my_blog/my_blog/settings.py
@@ -1,5 +1,5 @@
 """
-Django settings for mysite project.
+Django settings for my_blog project.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/topics/settings/
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')*8ivw-c4*^ct_$#!p*zr%zxp-$t$q4gkl_u=jazd$3c9y%gwy'
+SECRET_KEY = '@!)9vudkh3_-gcuki*9rv64*rb2uj_p_ui^j*2+jztg)4w7_oq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -36,7 +36,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'polls',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -48,9 +47,9 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'mysite.urls'
+ROOT_URLCONF = 'my_blog.urls'
 
-WSGI_APPLICATION = 'mysite.wsgi.application'
+WSGI_APPLICATION = 'my_blog.wsgi.application'
 
 
 # Database
@@ -58,10 +57,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backhend.postgresql.psycopg2',
-        'NAME':'mysite',
-	'USER':'postgres',
-	'PASSWORD':'postgres',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
